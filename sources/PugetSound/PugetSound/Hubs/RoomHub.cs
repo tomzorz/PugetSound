bcaa1336667f75afd0c12ad51a10f4d04ea95fdf
Tamás Deme <tomzorz@outlook.com>
2020-08-28 00:21:16 +0200

    added chat; extended the how it works page; added more comments to roomhub

--- sources/PugetSound/PugetSound/Hubs/RoomHub.cs
+++ sources/PugetSound/PugetSound/Hubs/RoomHub.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.SignalR;
 using Microsoft.Extensions.Logging;
@@ -23,12 +25,14 @@ namespace PugetSound.Hubs
 
         public override async Task OnConnectedAsync()
         {
+            // ensure room
             var username = Context.User.Claims.GetSpotifyUsername();
 
             var hasRoom = _roomService.TryGetRoomForUsername(username, out var room);
 
             if (!hasRoom) return;
 
+            // add to room group
             await Groups.AddToGroupAsync(Context.ConnectionId, room.RoomId);
         }
 
@@ -39,16 +43,19 @@ namespace PugetSound.Hubs
 
         public async Task Hello(long clientTime)
         {
+            // ensure room
             var username = Context.User.Claims.GetSpotifyUsername();
 
             var hasRoom = _roomService.TryGetRoomForUsername(username, out var room);
 
             if (!hasRoom) return;
 
+            // assign connection ID
             room.Members.First(x => x.UserName == username).ConnectionId = Context.ConnectionId;
 
             _logger.Log(LogLevel.Information, "{User} saying hello", username);
 
+            // send down state
             var (listeners, djs) = room.Members.SplitMembers();
 
             await Clients.Caller.ListenersChanged(listeners);
@@ -58,17 +65,20 @@ namespace PugetSound.Hubs
 
             await Clients.Caller.UpdateReactionTotals(room.CurrentReactionTotals.ToClientReactionTotals());
 
+            // play song if mid-song
             if (room.CurrentRoomState.IsPlayingSong) await Clients.Caller.SongChanged(room.CurrentRoomState);
         }
 
         public Task LeaveRoom()
         {
+            // ensure room
             var username = Context.User.Claims.GetSpotifyUsername();
 
             var hasRoom = _roomService.TryGetRoomForUsername(username, out var room);
 
             if (!hasRoom) return Task.CompletedTask;
 
+            // signal leave
             room.MemberLeave(room.Members.FirstOrDefault(x => x.UserName == username));
 
             return Task.CompletedTask;
@@ -76,6 +86,7 @@ namespace PugetSound.Hubs
 
         public Task BecomeDj()
         {
+            // ensure room
             var username = Context.User.Claims.GetSpotifyUsername();
 
             var hasRoom = _roomService.TryGetRoomForUsername(username, out var room);
@@ -84,6 +95,7 @@ namespace PugetSound.Hubs
 
             _logger.Log(LogLevel.Information, "{Username} became DJ in {Room}", username, room.RoomId);
 
+            // set to dj
             room.ToggleDj(room.Members.First(x => x.UserName == username), true);
 
             return Task.CompletedTask;
@@ -91,6 +103,7 @@ namespace PugetSound.Hubs
 
         public Task BecomeListener()
         {
+            // ensure room
             var username = Context.User.Claims.GetSpotifyUsername();
 
             var hasRoom = _roomService.TryGetRoomForUsername(username, out var room);
@@ -99,6 +112,7 @@ namespace PugetSound.Hubs
 
             _logger.Log(LogLevel.Information, "{Username} became listener in {Room}", username, room.RoomId);
 
+            // set to listener
             room.ToggleDj(room.Members.First(x => x.UserName == username), false);
 
             return Task.CompletedTask;
@@ -106,6 +120,7 @@ namespace PugetSound.Hubs
 
         public Task VoteSkipSong()
         {
+            // ensure room
             var username = Context.User.Claims.GetSpotifyUsername();
 
             var hasRoom = _roomService.TryGetRoomForUsername(username, out var room);
@@ -114,6 +129,7 @@ namespace PugetSound.Hubs
 
             _logger.Log(LogLevel.Information, "{Username} voted to skip song in {Room}", username, room.RoomId);
 
+            // vote skip
             room.VoteSkipSong(room.Members.First(x => x.UserName == username));
 
             return Task.CompletedTask;
@@ -121,6 +137,7 @@ namespace PugetSound.Hubs
 
         public async Task AddToLiked()
         {
+            // ensure room
             var username = Context.User.Claims.GetSpotifyUsername();
 
             var hasRoom = _roomService.TryGetRoomForUsername(username, out var room);
@@ -129,17 +146,20 @@ namespace PugetSound.Hubs
 
             _logger.Log(LogLevel.Information, "{Username} in {Room} added the current song to their liked songs", username, room.RoomId);
 
+            // add to liked
             await room.AddToLiked(room.Members.First(x => x.UserName == username));
         }
 
         public Task ReactionPressed(string reaction)
         {
+            // ensure room
             var username = Context.User.Claims.GetSpotifyUsername();
 
             var hasRoom = _roomService.TryGetRoomForUsername(username, out var room);
 
             if (!hasRoom) return Task.CompletedTask;
 
+            // mark reaction
             var didChangeReactionState = room.UserReaction(room.Members.First(x => x.UserName == username), reaction);
 
             if (!didChangeReactionState) return Task.CompletedTask;
@@ -147,6 +167,45 @@ namespace PugetSound.Hubs
             _logger.Log(LogLevel.Information, "{Username} in {Room} reacted to the current song with {Reaction}", username, room.RoomId, reaction);
 
             return Task.CompletedTask;
+        }
+
+        private static readonly ConcurrentDictionary<string, DateTimeOffset> ConnectionTimeouts = new ConcurrentDictionary<string, DateTimeOffset>();
+
+        private static readonly TimeSpan ChatTimeout = TimeSpan.FromSeconds(3.0);
+
+        public async Task SendMessage(string message)
+        {
+            // no empty messages
+            if (string.IsNullOrWhiteSpace(message)) return;
+
+            // no too long messages
+            if (message.Length > 90) return;
+
+            // ensure room
+            var username = Context.User.Claims.GetSpotifyUsername();
+
+            var hasRoom = _roomService.TryGetRoomForUsername(username, out var room);
+
+            if (!hasRoom) return;
+
+            // ensure value
+            if (!ConnectionTimeouts.ContainsKey(Context.ConnectionId))  ConnectionTimeouts[Context.ConnectionId] = DateTimeOffset.MinValue;
+
+            var hasTimeout = ConnectionTimeouts.TryGetValue(Context.ConnectionId, out var timeout);
+            if (!hasTimeout) return;
+
+            if (DateTimeOffset.Now - timeout > ChatTimeout)
+            {
+                // send message
+                await Clients.Group(room.RoomId).Chat(room.Members.First(x => x.UserName == username).FriendlyName, message);
+
+                // set new timeout
+                ConnectionTimeouts[Context.ConnectionId] = DateTimeOffset.Now;
+            } else
+            {
+                // pls wait
+                await Clients.Caller.Chat("[system]", "Chat disabled for 3 seconds.");
+            }
         }
     }
 }
